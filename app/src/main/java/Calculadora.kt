510f32ccc82bab86f2e6fbe0fe11c5ec7a4e91fc
gabrielrvb <live.rodrigues@hotmail.com>
2023-03-13 22:36:48 -0300

    uffa

---
+++ app/src/main/java/Calculadora.kt
@@ -0,0 +1,16 @@
+import java.lang.IllegalArgumentException
+
+class Calculadora {
+
+    fun sum(a: Double, b: Double): Double = a + b
+
+    fun subtract(a: Double, b: Double): Double = a - b
+
+    fun multiply(a: Double, b: Double): Double = a * b
+
+    fun divide(a: Double, b: Double): Double {
+        if (b == 0.0) throw IllegalArgumentException("Divisao por zero nao pode....")
+        return a / b
+    }
+
+}
